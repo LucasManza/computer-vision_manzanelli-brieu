ce435ca64d12b55ce955227bffe4b1887a7c4a73
Lucas Manzanelli <lucas.manzanelli@ing.austral.edu.ar>
2020-03-18 21:24:59 -0300

    Fix added trackbar to threshold ex 1.

--- src/practice/threshold/threshold-exercise-1.py
+++ src/practice/threshold/threshold-exercise-1.py
@@ -6,19 +6,28 @@ import cv2
 
 
 # Show normal, binary and binary inverted images.
-def show_binary_images(image):
+def show_binary_images(image, threshold: int):
     gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    ret, binary = cv2.threshold(gray_frame, 127, 255, cv2.THRESH_BINARY)
-    ret, binary_inv = cv2.threshold(gray_frame, 127, 255, cv2.THRESH_BINARY_INV)
+    ret, binary = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
+    ret, binary_inv = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY_INV)
 
     cv2.imshow('original', gray_frame)
     cv2.imshow('binary', binary)
     cv2.imshow('binary_inv', binary_inv)
 
 
+def nothing(x):
+    pass
+
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
+
+    threshold = 127
+
+    cv2.namedWindow("Window", cv2.WINDOW_AUTOSIZE)
+    cv2.createTrackbar("Threshold", "Window", 127, 255, nothing)
 
     while True:
 
@@ -28,11 +37,13 @@ if __name__ == '__main__':
         # Capture frame-by-frame
         ret, frame = cap.read()
 
+        threshold = cv2.getTrackbarPos("Threshold", "Window")
+        print(threshold)
+
         # Display the resulting frame
         cv2.imshow('frame', frame)
 
-        if cv2.waitKey(1) == ord('i'):
-            show_binary_images(frame)
+        show_binary_images(frame, threshold)
 
     # When everything done, release the capture
     cap.release()
