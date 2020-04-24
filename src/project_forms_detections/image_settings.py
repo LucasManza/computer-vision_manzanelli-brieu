328213cd2fefda4de28d1e3b3d11e0056a2dd03d
Lucas Manzanelli <lucas.manzanelli@ing.austral.edu.ar>
2020-04-24 12:24:17 -0300

    Remove un used poly detection.

--- src/project_forms_detections/image_settings.py
+++ src/project_forms_detections/image_settings.py
@@ -5,7 +5,6 @@ class ImageSettings:
     def __init__(self, window_name, threshold=127, morph_struct_size=1):
         self.threshold = threshold
         self.morph_struct_size = morph_struct_size if morph_struct_size > 1 else 1
-        self.approx_poly_dp = 1
         self.window_name: str = window_name
         self.__generate_trackbars__()
 
@@ -16,7 +15,6 @@ class ImageSettings:
         cv2.namedWindow(self.window_name, cv2.WINDOW_AUTOSIZE)
         cv2.createTrackbar("Threshold", self.window_name, self.threshold, 255, self.__nothing__)
         cv2.createTrackbar("Structure Size", self.window_name, self.morph_struct_size, 50, self.__nothing__)
-        cv2.createTrackbar("AproxPolyDP", self.window_name, self.approx_poly_dp, 50, self.__nothing__)
 
     def __update_values__(self):
         self.threshold = cv2.getTrackbarPos("Threshold", self.window_name)
