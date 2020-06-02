8abe0d1ab960ae43d3c309a0b1c45cdea01feb8c
LucasManza <lucas.manzanelli@ing.austral.edu.ar>
2020-06-01 22:01:56 -0300

    Camera web cam.

--- src/project_robotic_vision/main.py
+++ src/project_robotic_vision/main.py
@@ -3,7 +3,7 @@ import cv2
 from practice.homography_practice.homography_tool import HomographyTool
 
 homographyTool = HomographyTool()
-
+web_cam_img_name = "WebCam"
 
 def on_click(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -16,8 +16,8 @@ if __name__ == '__main__':
 
     cap = cv2.VideoCapture(0)
 
-    cv2.namedWindow("WebCam")
-    cv2.setMouseCallback('WebCam', on_click)
+    cv2.namedWindow(web_cam_img_name)
+    cv2.setMouseCallback(web_cam_img_name, on_click)
 
     while True:
         if cv2.waitKey(1) == ord('q'):
@@ -30,10 +30,10 @@ if __name__ == '__main__':
         if homographyTool.__2DPoints__.__len__() == 4:
             homo_img = homographyTool.four_point_transform(frame)
             # todo missing fixing camera calibration
-            
+
             cv2.imshow('Homography', homo_img)
             show_img = homographyTool.draw_points(frame)
 
-        cv2.imshow('WebCam', show_img)
+        cv2.imshow(web_cam_img_name, show_img)
 
 cv2.destroyAllWindows()
