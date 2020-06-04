3243dd3cbe9838fbe6238130f26ddf9a3b9c7402
Axel-brieu <62153637+Axel-brieu@users.noreply.github.com>
2020-06-04 18:34:17 +0200

    Fully operational, fixed some issues

--- src/practice/detect and extract.py
+++ src/practice/detect and extract.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #Trackbar parameters
 max_value = 255
-max_value_H = 360//2
+max_value_H = 360
 low_H = 0
 low_S = 0
 low_V = 0
@@ -18,51 +18,51 @@ def on_low_H_thresh_trackbar(val):
     global high_H
     low_H = val
     low_H = min(high_H-1, low_H)
-    cv.setTrackbarPos("Lowest_H_value", window_name, low_H)
+    cv.setTrackbarPos("Lowest_H", window_name, low_H)
     
 def on_high_H_thresh_trackbar(val):
     global low_H
     global high_H
     high_H = val
     high_H = max(high_H, low_H+1)
-    cv.setTrackbarPos("Highest_H_value", window_name, high_H)
+    cv.setTrackbarPos("Highest_H", window_name, high_H)
     
 def on_low_S_thresh_trackbar(val):
     global low_S
     global high_S
     low_S = val
     low_S = min(high_S-1, low_S)
-    cv.setTrackbarPos("Lowest_S_value", window_name, low_S)
+    cv.setTrackbarPos("Lowest_S", window_name, low_S)
     
 def on_high_S_thresh_trackbar(val):
     global low_S
     global high_S
     high_S = val
     high_S = max(high_S, low_S+1)
-    cv.setTrackbarPos("Highest_S_value", window_name, high_S)
+    cv.setTrackbarPos("Highest_S", window_name, high_S)
     
 def on_low_V_thresh_trackbar(val):
     global low_V
     global high_V
     low_V = val
     low_V = min(high_V-1, low_V)
-    cv.setTrackbarPos("Lowest_V_value", window_name, low_V)
+    cv.setTrackbarPos("Lowest_V", window_name, low_V)
     
 def on_high_V_thresh_trackbar(val):
     global low_V
     global high_V
     high_V = val
     high_V = max(high_V, low_V+1)
-    cv.setTrackbarPos("Highest_V_value", window_name, high_V)
+    cv.setTrackbarPos("Highest_V", window_name, high_V)
     
 cv.namedWindow(window_name)
 
-cv.createTrackbar("Lowest_H_value", window_name , low_H, max_value_H, on_low_H_thresh_trackbar)
-cv.createTrackbar("Highest_H_value", window_name , high_H, max_value_H, on_high_H_thresh_trackbar)
-cv.createTrackbar("Lowest_S_value", window_name , low_S, max_value, on_low_S_thresh_trackbar)
-cv.createTrackbar("Highest_S_value", window_name , high_S, max_value, on_high_S_thresh_trackbar)
-cv.createTrackbar("Lowest_V_value", window_name , low_V, max_value, on_low_V_thresh_trackbar)
-cv.createTrackbar("Highest_V_value", window_name , high_V, max_value, on_high_V_thresh_trackbar)
+cv.createTrackbar("Lowest_H", window_name , low_H, max_value_H, on_low_H_thresh_trackbar)
+cv.createTrackbar("Highest_H", window_name , high_H, max_value_H, on_high_H_thresh_trackbar)
+cv.createTrackbar("Lowest_S", window_name , low_S, max_value, on_low_S_thresh_trackbar)
+cv.createTrackbar("Highest_S", window_name , high_S, max_value, on_high_S_thresh_trackbar)
+cv.createTrackbar("Lowest_V", window_name , low_V, max_value, on_low_V_thresh_trackbar)
+cv.createTrackbar("Highest_V", window_name , high_V, max_value, on_high_V_thresh_trackbar)
 
 def detect(image):
     frame_HSV = cv.cvtColor(image, cv.COLOR_BGR2HSV)
@@ -70,13 +70,34 @@ def detect(image):
     return(frame_threshold)
 
 def extract(image):
-    frame_gray=cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    ret,labels,stats,centroids=cv.connectedComponentsWithStats(frame_gray)
+    frame=detect(image)
+    [ret,labels,stats,centroids]=cv.connectedComponentsWithStats(frame)
+    print(stats)
     areas=stats[:,4]
+    for i in range(len(areas)):#To avoid extracting the whole image's center
+        x=areas[i]
+        if x>800000:
+            areas[i]=0
     k=np.where(areas==max(areas))#Find connected comp with max area
-    k=zip(k)
+    print(k)
+    k=int(k[0])
+    print(centroids)
     x=centroids[k,0]
-    y=centroids[k,0]
+    y=centroids[k,1]
     center=(x,y)#Extract it's center
     area=areas[k]#Extract it's area
     return(center,area)
+
+#Test routine
+frame=cv.imread("Test.jpg")
+cv.imshow("frame", frame)
+while True:
+    cv.imshow("cal", detect(frame))
+    if cv.waitKey(0) & 0xFF==13:
+        k=extract(frame)
+        print(k)
+        cv.circle(frame,(int(k[0][0]),int(k[0][1])),10,(0,0,255))
+        cv.imshow("circle",frame)
+        cv.waitKey()
+        break
+cv.destroyAllWindows()
