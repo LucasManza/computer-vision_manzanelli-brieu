a34a53df6923c9d602e3d14cda153dfd9f9d2b06
LucasManza <lucas.manzanelli@ing.austral.edu.ar>
2020-06-21 19:47:08 -0300

    Added parse centimeters functions.

---
+++ src/project_robotic_vision/detect_annotations.py
@@ -0,0 +1,84 @@
+import cv2
+
+from project_forms_detections.image_operators import contours_operators
+
+
+def __points_to_rect_coords__(top_left: (int, int), bottom_right: (int, int)):
+    top_right = (bottom_right[0], top_left[1])
+    bottom_left = (top_left[0], bottom_right[1])
+
+    rect_coords = top_left, top_right, bottom_right, bottom_left
+    center_x = ((bottom_right[0] - top_left[0]) / 2) + top_left[0]
+    center_y = ((bottom_right[1] - top_left[1]) / 2) + top_left[1]
+    center_coord = int(center_x), int(center_y)
+    return rect_coords, center_coord
+
+
+def draw_system_ref(image, color):
+    h, w, c = image.shape
+    rect, center = __points_to_rect_coords__((0, 0), (w, h))
+    (tl, tr, br, bl) = rect
+
+    thickness = 2
+    cv2.rectangle(image, tl, center, color, thickness)
+    cv2.rectangle(image, center, br, color, thickness)
+    cv2.rectangle(image, tl, br, color, thickness)
+
+
+def __text_annotation__(img, text: str, size: (int, int), color):
+    x, y = size
+
+    font_scale = 0.5
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    # get the width and height of the text box
+    (text_width, text_height) = cv2.getTextSize(text, font, fontScale=font_scale, thickness=1)[0]
+    # set the text start position
+    text_offset_y = y - 10
+    # make the coords of the box with a small padding of two pixels
+    box_coords = ((x, text_offset_y), (x + text_width + 2, text_offset_y - text_height - 2))
+    cv2.rectangle(img, box_coords[0], box_coords[1], color, cv2.FILLED)
+    cv2.putText(img, text, (x, text_offset_y), font, fontScale=font_scale, color=(255, 255, 255), thickness=1)
+
+
+def __parse_text_annotation__(center: (int, int), image, centimeters: int) -> str:
+    unit: str = 'px'
+    x, y = center
+    if centimeters is not None:
+        unit = 'cm'
+        h, w = image.shape[:2]
+        x = int(center[0] / w * centimeters)
+        y = int(center[1] / h * centimeters)
+    return '(' + str(x) + unit + ', ' + str(y) + unit + ')'
+
+
+def __apply_homo_matrix__(homo_matrix, matrix2D):
+    matrix3D = matrix2D[0], matrix2D[1], 1
+    # Apply homo. matrix transformation over center
+    matrix3D = homo_matrix.dot(matrix3D)
+    return int(matrix3D[0]), int(matrix3D[1])
+
+
+def draw_annotations(img, contours, color, transf_matrix=None, centimeters: int = None):
+    """"
+    Apply a rectangle detection by contours, instead of drawing the contour per se.
+    """
+    copy_img = img.copy()
+    for cont in contours:
+        # Contours rect
+        x, y, w, h = cv2.boundingRect(cont)
+        # Get center
+        center = contours_operators.contour_center(cont)
+
+        # Apply Transformation
+        if transf_matrix is not None:
+            center = __apply_homo_matrix__(transf_matrix, center)
+            x, y = __apply_homo_matrix__(transf_matrix, (x, y))
+
+        # Draw center
+        cv2.circle(copy_img, center, color=color, radius=0, thickness=5)
+        # Draw rectangle container with text
+        cv2.rectangle(copy_img, (x, y), (x + w, y + h), (36, 255, 12), 1)
+        txt = __parse_text_annotation__(center, img, centimeters)
+        __text_annotation__(copy_img, txt, (x, y), color)
+
+    return copy_img
--- src/project_robotic_vision/vision_detector.py
+++ src/project_robotic_vision/vision_detector.py
@@ -5,6 +5,7 @@ from project_forms_detections.colours.rgb.colours import red_colour, green_colou
 from project_forms_detections.image_operators import threshold_operators, contours_operators
 from project_forms_detections.image_operators.contours_operators import DetectionContourEnum
 from project_forms_detections.image_operators import morphological_operators as  morph_operators
+from project_robotic_vision.detect_annotations import draw_system_ref, draw_annotations
 
 
 def __show_contours__(image, contours):
@@ -18,81 +19,11 @@ def __select_img__(select_bin: bool, img, bin_img, contours):
         return __show_contours__(img, contours)
 
 
-def __text_annotation__(img, text: str, size: (int, int), color):
-    x, y = size
-
-    font_scale = 0.5
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    # get the width and height of the text box
-    (text_width, text_height) = cv2.getTextSize(text, font, fontScale=font_scale, thickness=1)[0]
-    # set the text start position
-    text_offset_y = y - 10
-    # make the coords of the box with a small padding of two pixels
-    box_coords = ((x, text_offset_y), (x + text_width + 2, text_offset_y - text_height - 2))
-    cv2.rectangle(img, box_coords[0], box_coords[1], color, cv2.FILLED)
-    cv2.putText(img, text, (x, text_offset_y), font, fontScale=font_scale, color=(255, 255, 255), thickness=1)
-
-
-def __apply_homo_matrix__(homo_matrix, matrix2D):
-    matrix3D = matrix2D[0], matrix2D[1], 1
-    # Apply homo. matrix transformation over center
-    matrix3D = homo_matrix.dot(matrix3D)
-    return int(matrix3D[0]), int(matrix3D[1])
-
-
-def __draw_annotations__(img, contours, color, transf_matrix=None):
-    """"
-    Apply a rectangle detection by contours, instead of drawing the contour per se.
-    """
-    copy_img = img.copy()
-    for cont in contours:
-        # Contours rect
-        x, y, w, h = cv2.boundingRect(cont)
-        # Get center
-        center = contours_operators.contour_center(cont)
-
-        # Apply Transformation
-        if transf_matrix is not None:
-            center = __apply_homo_matrix__(transf_matrix, center)
-            # h, w = __apply_homo_matrix__(transf_matrix, (h, w))
-            x, y = __apply_homo_matrix__(transf_matrix, (x, y))
-
-        # Draw center
-        cv2.circle(copy_img, center, color=color, radius=0, thickness=5)
-        # Draw rectangle container with text
-        cv2.rectangle(copy_img, (x, y), (x + w, y + h), (36, 255, 12), 1)
-        __text_annotation__(copy_img, str(center), (x, y), color)
-
-    return copy_img
-
-
-def __show_shapes_detection__(camera_frame, contours_result, transf_matrix):
+def __show_shapes_detection__(camera_frame, contours_result, transf_matrix, centimeters):
     img_detection = camera_frame
     if contours_result.__len__() > 0:
-        img_detection = __draw_annotations__(camera_frame, contours_result, green_colour, transf_matrix)
+        img_detection = draw_annotations(camera_frame, contours_result, green_colour, transf_matrix, centimeters)
     return img_detection
-
-
-def __points_to_rect_coords__(top_left: (int, int), bottom_right: (int, int)):
-    top_right = (bottom_right[0], top_left[1])
-    bottom_left = (top_left[0], bottom_right[1])
-
-    rect_coords = top_left, top_right, bottom_right, bottom_left
-    center_x = ((bottom_right[0] - top_left[0]) / 2) + top_left[0]
-    center_y = ((bottom_right[1] - top_left[1]) / 2) + top_left[1]
-    center_coord = int(center_x), int(center_y)
-    return rect_coords, center_coord
-
-
-def __draw_system_ref__(image, color):
-    h, w, c = image.shape
-    rect, center = __points_to_rect_coords__((0, 0), (w, h))
-    (tl, tr, br, bl) = rect
-
-    thickness = 2
-    cv2.rectangle(image, tl, center, color, thickness)
-    cv2.rectangle(image, center, br, color, thickness)
-    cv2.rectangle(image, tl, br, color, thickness)
 
 
 def detector_target(
@@ -141,8 +72,9 @@ def detector_target(
     contours_result = contours_operators.filter_by_area(contours_result, 100, 10000)
 
     # It's show a new window all possible results
-    camera_image = __show_shapes_detection__(camera_image, contours_result, None)
-    homo_image = __show_shapes_detection__(homo_image, contours_result, homo_matrix)
-    __draw_system_ref__(camera_image, (255, 0, 0))
+    camera_image = __show_shapes_detection__(camera_image, contours_result, transf_matrix=None, centimeters=None)
+    homo_image = __show_shapes_detection__(homo_image, contours_result, transf_matrix=homo_matrix, centimeters=15)
+    draw_system_ref(camera_image, (255, 0, 0))
+    draw_system_ref(homo_image, (255, 0, 0))
 
     return camera_image, homo_image
