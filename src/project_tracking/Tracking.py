f7d7ec52baafd92d5a9800dda9e3f1dd966934b8
Axel-brieu <axel.brieu@gadz.org>
2020-06-25 22:51:45 +0200

    Fixed slowness issue

--- src/project_tracking/Tracking.py
+++ src/project_tracking/Tracking.py
@@ -5,6 +5,7 @@ import numpy as np
 video = cv.VideoCapture("carsRt9_3.avi")
 prev=video.read()[1]
 tracked=None
+count=1
 
 
 def movement(image1,image2):#Gives back a binary image of what moved between two frames
@@ -35,27 +36,32 @@ def get_distance(bbox1, bbox2):#Computes distance in pixels between two bounding
     return(abs(distance))
 
 def add_or_update(contours,tracked, image):
-    new_index=len(tracked)
+    new_index=len(tracked)+1
+    for t in tracked:
+        tracker=t["tracker"]
+        success,t["bbox"]=tracker.update(image)
+        
     for c in contours:
         for t in tracked:#Compare each detected contour with already tracked objects
-            if get_distance(c.get("bbox"),t.get("bbox"))<5:#Change for bounding boxes
-                tracker=t.get("tracker")
-                sucess,t["bbox"]=tracker.update(image)
-                if not(sucess):#Tracker failed: object no longer tracked
-                    t["updated"]=False
+            if get_distance(c.get("bbox"),t.get("bbox"))<200:
+                t["updated"]=True
+                break
                     
-            else:#New contour
-                tracker=cv.TrackerKCF_create()#Create a new tracker
-                c["tracker"]=tracker
-                sucess=tracker.init(image,c["bbox"])
-                if not(sucess):#Verify init
-                    c["updated"]=False
-                c["index"]=new_index
-                tracked.append(c)
-        
+        tracker=cv.TrackerKCF_create()#Create a new tracker
+        c["tracker"]=tracker
+        sucess=tracker.init(image,c["bbox"])
+        if not(sucess):#Verify init
+            c["updated"]=False
+        c["index"]=new_index
+        tracked.append(c)
+    
+    i=0    
     for x in tracked:
         if not(x["updated"]):#Everything that failed is removed
             tracked.remove(x)
+            i+=1
+    print("successfully removed")
+    print(i)
     for i in range(len(tracked)):#Reorder index after removing elements
         tracked[i]["index"]=i
     return(tracked)
@@ -63,6 +69,8 @@ def add_or_update(contours,tracked, image):
 
 while True:
     frame=video.read()[1]
+    print("Loop n°")
+    print(count)
     
     if frame is None:#Test if we reached the end of the video
             break
@@ -76,11 +84,14 @@ while True:
     
     contours, hierarchy = cv.findContours(binary, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     filtered_contours=[]
-    index=0
+    index=1
     for c in contours:#Filter smaller contours to avoid false positives
         if cv.contourArea(c)>900:
             a={"index": index, "bbox": cv.boundingRect(c), "tracker": None, "updated": True}
             filtered_contours.append(a)
+            index+=1
+    print("Objects detected")
+    print(len(filtered_contours))
             
     #frame=cv.drawContours(frame, filtered_contours, -1, (0,255,0))
     if tracked!=None:#Main case
@@ -93,11 +104,16 @@ while True:
             sucess=tracker.init(frame,t["bbox"])
             if not(sucess):#Verify init
                 t["updated"]=False
+    print("Tracked objects")
+    print(len(tracked))
         
     #Missing a function to draw results on frame
     cv.imshow("contours",frame)
     
+    for t in tracked:
+        t["updated"]=False
     prev=frame
+    count+=1
     key = cv.waitKey(30)
     if key == ord('q') or key == 27:#Quit
         print("Keyboard interrupt")
