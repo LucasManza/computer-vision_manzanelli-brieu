399ea62aebdddf29d01ad4e8813ecfa50ec47ecc
LucasManza <lucas.manzanelli@ing.austral.edu.ar>
2020-07-31 20:09:09 -0300

    rendering missing.

--- src/project_ar/demo_detect_qr_2.py
+++ src/project_ar/demo_detect_qr_2.py
@@ -1,6 +1,10 @@
+import math
+import os
+
 import cv2
 import numpy as np
 
+from project_ar.obj_model import OBJ
 from project_ar.trackbar_generator import TrackbarSettings
 
 GREEN_COLOR = (0, 255, 0)
@@ -44,15 +48,93 @@ def __nothing__(self, x):
     pass
 
 
+def render(img, obj, projection, model, color=False):
+    vertices = obj.vertices
+    scale_matrix = np.eye(3) * 3
+    h, w, c = model.shape
+
+    for face in obj.faces:
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+        points = np.dot(points, scale_matrix)
+        # render model in the middle of the reference surface. To do so,
+        # model points must be displaced
+        points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)
+        imgpts = np.int32(dst)
+        if color is False:
+            cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
+        else:
+            color = hex_to_rgb(face[-1])
+            color = color[::-1]  # reverse
+            cv2.fillConvexPoly(img, imgpts, color)
+
+    return img
+
+
+def hex_to_rgb(hex_color):
+    """
+    Helper function to convert hex strings to RGB
+    """
+    hex_color = hex_color.lstrip('#')
+    h_len = len(hex_color)
+    return tuple(int(hex_color[i:i + h_len // 3], 16) for i in range(0, h_len, h_len // 3))
+
+
+def projection_matrix(camera_parameters, homography):
+    """
+     From the camera calibration matrix and the estimated homography
+     compute the 3D projection matrix
+     """
+
+    # Compute rotation along the x and y axis as well as the translation
+    homography = homography * (-1)
+    rot_and_transl = np.dot(np.linalg.inv(camera_parameters), homography)
+    col_1 = rot_and_transl[:, 0]
+    col_2 = rot_and_transl[:, 1]
+    col_3 = rot_and_transl[:, 2]
+    # normalise vectors
+    l = math.sqrt(np.linalg.norm(col_1, 2) * np.linalg.norm(col_2, 2))
+    rot_1 = col_1 / l
+    rot_2 = col_2 / l
+    translation = col_3 / l
+    # compute the orthonormal basis
+    c = rot_1 + rot_2
+    p = np.cross(rot_1, rot_2)
+    d = np.cross(c, p)
+    rot_1 = np.dot(c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_2 = np.dot(c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2), 1 / math.sqrt(2))
+    rot_3 = np.cross(rot_1, rot_2)
+    # finally, compute the 3D projection matrix from the model to the current frame
+    projection = np.stack((rot_1, rot_2, rot_3, translation)).T
+    return np.dot(camera_parameters, projection)
+
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
     main_window_name: str = 'Webcam'
-    threshold = 127
 
-    threshold_trackbar = TrackbarSettings('Settings')
-    morph_trackbar = TrackbarSettings('Settings', 'Morph Size Struct', 2, 30)
+    obj = OBJ("../../src/assets/99-intergalactic_spaceship-obj/Intergalactic_Spaceship-(Wavefront).obj",
+              swapyz=True)
 
     qr_target = cv2.imread('../../src/assets/QR_1.png')
+    camera_parameters = np.array([
+        [
+            539.5314058058052,
+            0.0,
+            213.90895733662572
+        ],
+        [
+            0.0,
+            543.5813662750779,
+            275.7850229389913
+        ],
+        [
+            0.0,
+            0.0,
+            1.0
+        ]
+    ])
 
     # percent by which the image is resized
     scale_percent = 30
@@ -89,15 +171,33 @@ if __name__ == '__main__':
         # frame_features = cv2.drawMatches(qr_target, key_points_target, cam_frame, key_points_frame, match_descriptors,
         #                                  None, flags=2)
 
-        if len(match_descriptors) > 20:
+        MIN = 30
+
+        if len(match_descriptors) > MIN:
+            # draw first 15 matches.
+            det_frame = cv2.drawMatches(qr_target, key_points_target, cam_frame, key_points_frame,
+                                        match_descriptors[:MIN], 0, flags=2)
+            cv2.imshow('Detect', det_frame)
+
             src_pts = np.float32([key_points_target[m.queryIdx].pt for m in match_descriptors]).reshape(-1, 1, 2)
             dst_pts = np.float32([key_points_frame[m.trainIdx].pt for m in match_descriptors]).reshape(-1, 1, 2)
-            matrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-            print(matrix)
 
-            pts = np.float32([[0, 0], [0, height_target], [width_target, height_target], [width_target, 0]])
-            pts = pts.reshape(-1, 1, 2)
-            dst = cv2.perspectiveTransform(pts, matrix)
-            cam_frame = cv2.polylines(cam_frame, [np.int32(dst)], True, (255, 0, 255), 3, cv2.LINE_AA)  # draw polylines
+            homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+            # Draw a rectangle that marks the found model in the frame
+            h, w, c = qr_target.shape
+            pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+            # project corners into frame
+            dst = cv2.perspectiveTransform(pts, homography)
+            # connect them with lines
+            frame = cv2.polylines(cam_frame, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+            # if a valid homography matrix was found render cube on model plane
+            if homography is not None:
+                # obtain 3D projection matrix from homography matrix and camera parameters
+                projection = projection_matrix(camera_parameters, homography)
+                # project cube or model
+                frame = render(frame, obj, projection, qr_target, False)
+                # frame = render(frame, model, projection)
+            cv2.imshow('frame', frame)
 
         cv2.imshow('Webcam', cam_frame)
