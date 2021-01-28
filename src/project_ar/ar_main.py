e85c2bc83e405190646311265ceb63362a203d23
Lucas Manzanelli <lucas.manzanelli@ing.austral.edu.ar>
2021-01-28 19:07:36 -0300

    Render small ship.

--- src/project_ar/ar_main.py
+++ src/project_ar/ar_main.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 from project_ar.obj_model import OBJ
-from project_ar.qr_detector import qr_detector
+from project_ar import qr_detector
 from project_ar.trackbar_generator import TrackbarSettings
 
 
@@ -18,64 +18,7 @@ def __bin_imag__(img, threshold, morph_struct_size):
     return bin_img
 
 
-def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype="float32")
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-    # return the ordered coordinates
-    return rect
-
-
-def rect_homography(image, rect):
-    # obtain a consistent order of the points and unpack them
-    # individually
-    rect = order_points(np.array(rect))
-    (tl, tr, br, bl) = rect
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-    # now that we have the dimensions of the new image, construct
-    # the set of destination points to obtain a "birds eye view",
-    # (i.e. top-down view) of the image, again specifying points
-    # in the top-left, top-right, bottom-right, and bottom-left
-    # order
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
-
-    # compute the perspective transform matrix and then apply it
-    transform_matrix = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, transform_matrix, (maxWidth, maxHeight))
-
-    # return the warped image
-    return warped, transform_matrix
-
-
-DEFAULT_COLOR = (0, 0, 0)
+DEFAULT_COLOR = (0, 255, 255)
 
 
 def hex_to_rgb(hex_color):
@@ -112,7 +55,7 @@ def render(img, obj, projection, target_height, target_width, color=False):
             cv2.fillConvexPoly(img, imgpts, color)
 
     return img
-
+    
 
 def projection_matrix(camera_parameters, homography):
     """
@@ -153,15 +96,17 @@ if __name__ == '__main__':
     morph_trackbar = TrackbarSettings('Settings', 'Morph Size Struct', 2, 30)
 
     qrDecoder = cv2.QRCodeDetector()
-    obj_dir = "../../assets/pirate-ship-fat.obj"
+    obj_dir = "../assets/pirate-ship-fat.obj"
     obj = OBJ(obj_dir, swapyz=True)
 
     camera_parameters = [[539.5314058058052, 0.0, 213.90895733662572],
                          [0.0, 543.5813662750779, 275.7850229389913],
                          [0.0, 0.0, 1.0]]
 
-    qr_image = cv2.imread("../../assets/QR_1.png")
-    h, w, _ = qr_image.shape
+    qr_image = cv2.imread("../assets/QR_1.png")
+    qr_height, qr_width, _ = qr_image.shape
+    qr_bin_frame = __bin_imag__(qr_image, threshold, 1)
+    original_bbox = qr_detector.__detect_bbox__(qrDecoder, bin_frame=qr_bin_frame)
 
     while True:
         if cv2.waitKey(1) == ord('q'): break
@@ -176,23 +121,26 @@ if __name__ == '__main__':
         bin_frame = __bin_imag__(cam_frame, threshold, morph_struct_size)
 
         # Detect QR bbox from frame
-        bbox = qr_detector.__detect_bbox__(qrDecoder, cam_frame, bin_frame)
+        bbox = qr_detector.__detect_bbox__(qrDecoder, bin_frame=bin_frame, cam_frame_draw=cam_frame)
 
         if bbox is not None:
-
-            warped_img, h_matrix = rect_homography(cam_frame, bbox)
+            src_pts = np.float32(original_bbox)
+            dst_pts = np.float32(bbox)
+            print(src_pts)
+            # warped_img, homography = rect_homography(cam_frame, bbox)
+            homography, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
             # obtain 3D projection matrix from homography matrix and camera parameters
-            projection = projection_matrix(camera_parameters, h_matrix)
+            projection = projection_matrix(camera_parameters, homography)
 
-            # if h_matrix is not None:
-            #     try:
-            #         # obtain 3D projection matrix from homography matrix and camera parameters
-            #         projection = projection_matrix(camera_parameters, h_matrix)
-            #
-            #         cam_frame = render(cam_frame, obj, projection, h, w, False)
-            #         # frame = render(frame, model, projection)
-            #     except:
-            #         pass
+            if homography is not None:
+                try:
+                    # obtain 3D projection matrix from homography matrix and camera parameters
+                    projection = projection_matrix(camera_parameters, homography)
+
+                    cam_frame = render(cam_frame, obj, projection, qr_height, qr_width, False)
+                    # frame = render(frame, model, projection)
+                except:
+                    pass
 
         cv2.imshow(main_window_name, cam_frame)
--- src/project_ar/qr_detector.py
+++ src/project_ar/qr_detector.py
@@ -17,16 +17,18 @@ def __draw_bbox__(cam_frame, top_right, bottom_right, bottom_left, top_left):
     __draw_coord__(cam_frame, top_left, RED_COLOR)
 
 
-def __detect_bbox__(qrDecoder, cam_frame, bin_frame):
+def __detect_bbox__(qrDecoder, bin_frame, cam_frame_draw=None):
     # Detect QR bbox from frame
     data, bbox, rectifiedImage = qrDecoder.detectAndDecode(bin_frame)
 
-    if bbox is not None:
-        top_right = tuple(bbox[0][0])
-        bottom_right = tuple(bbox[1][0])
-        bottom_left = tuple(bbox[2][0])
-        top_left = tuple(bbox[3][0])
-        __draw_bbox__(cam_frame, top_right, bottom_right, bottom_left, top_left)
-        return top_right, bottom_right, bottom_left, top_left
-    else:
-        return None
+    if bbox is None: return None
+
+    top_right = tuple(bbox[0][0])
+    bottom_right = tuple(bbox[1][0])
+    bottom_left = tuple(bbox[2][0])
+    top_left = tuple(bbox[3][0])
+
+    if cam_frame_draw is not None:
+        __draw_bbox__(cam_frame_draw, top_right, bottom_right, bottom_left, top_left)
+
+    return top_right, bottom_right, bottom_left, top_left
